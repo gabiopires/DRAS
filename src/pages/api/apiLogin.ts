924e32cdab805bace162000995a33d35e63f9bd3
Gabrielle <piresgaby0@gmail.com>
2026-06-16 19:26:22 -0300

    a

--- src/pages/api/apiLogin.ts
+++ src/pages/api/apiLogin.ts
@@ -38,7 +38,7 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
     const user = rows[0];
 
     //Compara a senha digitada em texto limpo com o hash salvo no banco
-    const senhaValida = true;
+    const senhaValida = await bcrypt.compare(senha, user.Senha);
 
     if (!senhaValida) {
       connection.release();
